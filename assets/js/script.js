83e805c4230c20c5829b5cb8276ac8a6085b2737
Wiliam Fery Sagala <sagala2002@pop-os.localdomain>
2023-02-18 01:18:13 +0700

    Fourth Commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+VanillaTilt.init(
+  document.querySelector(".box"),
+  document.querySelector(".bx2"),
+  {
+    max: 25,
+    speed: 400,
+    glare: true,
+  }
+);
+
+VanillaTilt.init(document.querySelector(".bx2"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+});
